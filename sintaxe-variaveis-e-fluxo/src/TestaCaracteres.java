0602dc567c2bfa0d7cb8e2866497be55d0c1cb3b
vi-lcandido <vitoria.lcandido@gmail.com>
2023-03-28 20:57:07 -0300

    soma de um int com char

--- sintaxe-variaveis-e-fluxo/src/TestaCaracteres.java
+++ sintaxe-variaveis-e-fluxo/src/TestaCaracteres.java
@@ -7,10 +7,18 @@ public class TestaCaracteres {
 		String palavra = "alura cursos online de tecnologia";
 		System.out.println(palavra);
 		
+		
 		//concatena o número com o texto transformando o número em string
 		palavra = palavra + 2020;
 		System.out.println(palavra);
 				
+		char valor = 65; //compila
+		System.out.println(valor); //printa A
+		
+//		valor = valor + 1; nao compila essa soma, jaba transforma para o maior tipo, que é int já que 1 é um inteiro. Pra compilar preciso do casting
+	 
+		
+		valor = (char)(valor + 1); // compila pois com char mostra que quero um resultado dessa soma em char
+		System.out.println(valor);
 	}
-
 }
