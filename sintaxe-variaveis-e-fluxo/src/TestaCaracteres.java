a9026c743858e6e359672e821a31ad9be138900e
vi-lcandido <vitoria.lcandido@gmail.com>
2023-03-28 20:48:14 -0300

    trabalhando com caracteres

---
+++ sintaxe-variaveis-e-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,16 @@
+
+public class TestaCaracteres {
+	public static void main (String[] args) {
+		char letra = 'a';
+		System.out.println(letra);
+		
+		String palavra = "alura cursos online de tecnologia";
+		System.out.println(palavra);
+		
+		//concatena o número com o texto transformando o número em string
+		palavra = palavra + 2020;
+		System.out.println(palavra);
+				
+	}
+
+}
