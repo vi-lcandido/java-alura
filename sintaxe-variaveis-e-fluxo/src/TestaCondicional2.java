febc879bbf5fb11d3187ff9ff0a09ca69b3f7c6b
vi-lcandido <vitoria.lcandido@gmail.com>
2023-03-28 21:19:17 -0300

    operador com condicional

---
+++ sintaxe-variaveis-e-fluxo/src/TestaCondicional2.java
@@ -0,0 +1,16 @@
+
+public class TestaCondicional2 {
+	public static void main(String[] args) {
+		System.out.println("testando condicionais");
+		int idade = 15;
+//		int quantidadePessoas = 1;
+		boolean acompanhado = true;
+		if (idade >= 18 || acompanhado) {
+			System.out.println("seja bem vindo");
+			
+		} else {	
+				System.out.println("não pode entrar");
+			}
+		}
+
+}
